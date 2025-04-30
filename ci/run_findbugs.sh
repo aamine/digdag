4cb960465d8fd893663f81b41f45a90ebf256e02
Minero Aoki <minero.aoki@treasure-data.com>
2025-04-30 13:12:00 +0900

    delete ci/ directory

--- ci/run_findbugs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-docker run \
--w /digdag \
--v `pwd`/:/digdag \
--v ~/.gradle:/root/.gradle \
-$BUILD_IMAGE \
-./gradlew spotbugsMain spotbugsTest --info --no-daemon
--- ci/run_test_h2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-$BASEDIR/run_test_docker.sh "$@"
